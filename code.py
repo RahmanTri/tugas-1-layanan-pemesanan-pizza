a54780fdb315c5531827e42947caaaa761c75ea9
RakaPhaedra <149665519+RakaPhaedra@users.noreply.github.com>
2023-11-02 21:56:20 +0700

    Update code.py

--- code.py
+++ code.py
@@ -47,3 +47,53 @@ if konfirmasi == "y":
     for i, daftar_size in enumerate(size):
         print(i+1, daftar_size)
     pilihan_size = int(input('Masukkan Size pizza pilihan anda (1-3) : '))
+
+
+# Akan menampilkan penawaran keju
+    print('==============================================')
+    keju_conf = input('Apakah Anda ingin menambah keju: (y/n) : ')
+    if keju_conf == "y":
+        keju = True
+    else:
+        keju = False      
+
+# program berhenti ketika input selain "y"
+else:
+    print('''
+==============================================
+  Terimakasih telah menggunakan layanan kami
+==============================================
+          ''')
+
+# program mulai melakukan pengecekan dan menghitung harga
+harga = 0
+
+# memberikan harga dasar berdasarkan ukuran pizza
+if pilihan_crust == 1: # harga jika harga jika crust no 4
+    harga += 43637 # tambahan harga jika ukuran personal
+    if pilihan_size == 1:
+        harga += 0
+    elif pilihan_size == 2:
+        harga += 57273 # tambahan harga jika ukuran regular
+    else:
+        harga += 89091 # tambahan harga jika ukuran large
+
+elif pilihan_crust == 2: # harga jika harga jika crust no 2
+    harga += 55455 #harga jika ukuran personal
+    if pilihan_size == 1:
+        harga += 0
+    elif pilihan_size == 2:
+        harga += 65455 # tambahan harga jika ukuran regular
+    else:
+        harga += 104545 # tambahan harga jika ukuran large
+
+elif pilihan_crust == 3: # harga jika harga jika crust no 3
+    harga += 52728 #harga jika ukuran personal
+    if pilihan_size == 1:
+        harga += 0
+    elif pilihan_size == 2:
+        harga += 64545 # tambahan harga jika ukuran regular
+    else:
+        harga += 102727# tambahan harga jika ukuran large
+
+elif pilihan_crust == 4: # harga jika crust no 4
